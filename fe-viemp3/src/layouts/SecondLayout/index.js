208a06715ce668d8499cdec7b03089f2fcab867e
QuangTrongLeo <trongphamtg05@gmail.com>
2026-04-24 08:30:23 +0700

    feat: impl SecondLayout component

--- fe-viemp3/src/layouts/SecondLayout/index.js
+++ fe-viemp3/src/layouts/SecondLayout/index.js
@@ -1,8 +1,24 @@
-// Layouts/HeaderOnlyLayout/index.js
 import React from 'react';
+import BaseLayout from '../BaseLayout';
+import styles from './SecondLayout.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 function SecondLayout({ children }) {
-  return <>{children}</>;
+  return (
+    <BaseLayout
+      renderMainContent={() => (
+        <div className={cx('container-fluid')} style={{ paddingTop: '80px' }}>
+          <div className={cx('row')} style={{ minHeight: 'calc(100vh - 80px)' }}>
+            <div className={cx('col-8', 'col-md-8', 'col-lg-8', 'mx-auto')}>
+              <div className={cx('content')}>{children}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    />
+  );
 }
 
 export default SecondLayout;
